746f82ee5e71cdf568b71bb9f9827640b87ee6a1
lala <hassetawoke100@gmail.com>
2025-05-01 23:29:00 +0300

    login

---
+++ FRONTEND/BUYERS/src/pages/Login.jsx
@@ -0,0 +1,23 @@
+ import React from "react";
+import { Route } from "react-router-dom";
+ function Login(){
+ <form>
+    
+ <input type="text"  placeholder="usernamae">
+</input>
+ 
+<input type="password"  placeholder="password">
+</input>
+ 
+
+
+   <h3> do u have an account ? </h3>
+   <Route>
+
+
+   </Route>
+ </form>
+ 
+
+ };
+  export default Login;
